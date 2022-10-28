0f6f733ca7833afbbb730fb522f1d4774657cf2a
Ha Gonin <97175351+hagonin@users.noreply.github.com>
2022-10-28 23:29:52 +0100

    modify config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
 		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
+		 screens: {
+		      sm: '480px',
+		      md: '768px',
+		      lg: '1200px',
+		},
 		extend: {},
 	},
 	plugins: [require('flowbite/plugin')],
