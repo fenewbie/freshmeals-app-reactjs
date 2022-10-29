db164d3b954dbfff5e223039025cdeacbef08b76
phantrunghieu91 <phantrunghieu1291@gmail.com>
2022-10-29 22:55:06 +0700

    added xl theme in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{js,jsx,ts,tsx}',
-		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-	],
-	theme: {
-		 screens: {
-		      sm: '480px',
-		      md: '768px',
-		      lg: '1200px',
-		},
-		extend: {},
-	},
-	plugins: [require('flowbite/plugin')],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1200px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
 };
