2ef4cc6f70308385f7534c2db46e0ce4ff0e4a87
Ha Gonin <97175351+hagonin@users.noreply.github.com>
2022-10-30 11:50:23 +0000

    update apiAxios

--- src/services/apiAxios.js
+++ src/services/apiAxios.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
 const baseURL =
-	'https://api.spoonacular.com';
+	'https://api.spoonacular.com/food/products';
+
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 const apiAxios = axios.create({
-	baseURL: baseURL
+	baseURL: baseURL,
+	headers: {
+		'x-api-key': API_KEY,
+		'Content-Type': 'application/json'
+	},
 });
 
 apiAxios.interceptors.request.use(async (config) => {
