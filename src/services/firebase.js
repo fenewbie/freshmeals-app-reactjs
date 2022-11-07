7a1d85d0388e2e669b8e3c7a9bee2ec92b2f689d
gonin <goninha@outlook.fr>
2022-11-07 15:14:45 +0000

    delete unnecessary files after merging from navbar

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -13,5 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-export default db;
+export const db = getFirestore(app);
