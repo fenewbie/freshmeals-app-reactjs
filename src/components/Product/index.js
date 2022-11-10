ac4fc75beea1fd362f368fc11683f3be7ce8b348
gonin <goninha@outlook.fr>
2022-11-10 14:49:14 +0000

    call API in progress

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -4,82 +4,73 @@ import { AiOutlineHeart } from 'react-icons/ai';
 import { AiTwotoneHeart } from 'react-icons/ai';
 import { FaShoppingCart } from 'react-icons/fa';
 
-import { cartActions } from '../../redux/products/productSlice';
-
 import Rating from './Rating';
-import { useDispatch } from 'react-redux';
-
+import { useEffect, useState } from 'react';
+import getProducts from '../../redux/products/productSlice';
 
 const ProductCard = (props) => {
-	const {
-		id,
-		title,
-		imageType,
-		to = '/',
-		label = '20%',
-		wishList,
-	} = props.item;
+	const [food, setFood] = useState([]);
+	getProducts(setFood);
 
-	const dispatch = useDispatch();
 	return (
 		<div className="border-2 border-zinc-100 transition-all duration-300 hover:shadow-2xl hover:shadow-gray-300 group">
-			<Link to={to} className={`block relative bg-slate-100 pt-[100%]`}>
-				{label && (
-					<span className="absolute top-4 right-4 text-sm font-bold text-white py-1 px-3 rounded-tl-2xl rounded-br-2xl bg-greenBtn">
-						{label}
-					</span>
-				)}
+			{food.map((item) => (
+				<div>
+					<Link className={`block relative bg-slate-100 pt-[100%]`}>
+						<span className="absolute top-4 right-4 text-sm font-bold text-white py-1 px-3 rounded-tl-2xl rounded-br-2xl bg-greenBtn">
+							NEW
+						</span>
+						)
+						<img
+							src={item.image}
+							title="product"
+							className="absolute top-0 left-0 w-full h-full object-contain"
+							alt={item.title}
+						/>
+						<div className="absolute left-0 top-1/2 w-full text-center hidden group-hover:block animate-[fadeOut_300ms_ease-out_both]">
+							<button
+								title=""
+								className="h-[50px] w-[50px] font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 mx-1"
+							>
+								<FaRegEye className="mx-auto" />
+							</button>
 
-				<img
-					src={imageType}
-					title="product"
-					className="absolute top-0 left-0 w-full h-full object-contain"
-                    alt={title}
-				/>
+							<button
+								title=""
+								className="h-[50px] w-[50px] font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 mx-1"
+							>
+								<FaShoppingCart className="mx-auto" />
+							</button>
 
-				<div className="absolute left-0 top-1/2 w-full text-center hidden group-hover:block animate-[fadeOut_300ms_ease-out_both]">
-					<button
-						title=""
-						className="h-[50px] w-[50px] font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 mx-1"
-					>
-						<FaRegEye className="mx-auto" />
-					</button>
+							<button
+								title=""
+								className="h-[50px] w-[50px] font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 mx-auto"
+							>
+									<AiTwotoneHeart className="mx-auto" />
+								{/* {wishList ? (
+								) : (
+									<AiOutlineHeart className="mx-auto" />
+								)} */}
+							</button>
+						</div>
+					</Link>
 
-					<button
-						title=""
-						className="h-[50px] w-[50px] font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 mx-1"
-					>
-						<FaShoppingCart className="mx-auto" />
-					</button>
-
-					<button
-						title=""
-						className="h-[50px] w-[50px] font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 mx-auto"
-					>
-						{wishList ? (
-							<AiTwotoneHeart className="mx-auto" />
-						) : (
-							<AiOutlineHeart className="mx-auto" />
-						)}
-					</button>
+					<div className="p-8">
+						<Rating />
+						<Link
+							className="text-[15px] text-center capitalize font-bold mt-1 block hover:text-greenBtn transition-all duration-300"
+						>
+							{item.title}
+						</Link>
+						<div className="flex justify-center mt-2">
+							<h4 className="text-xl font-bold text-greenBtn">£20.00</h4>
+							<h4 className="text-xl font-bold text-greenBtn line-through ml-3 opacity-60">
+								$25.00
+							</h4>
+						</div>
+					</div>
 				</div>
-			</Link>
-
-			<div className="p-8">
-				<Rating />
-				<Link
-					to={to}
-					className="text-[15px] text-center capitalize font-bold mt-1 block hover:text-greenBtn transition-all duration-300"
-				>
-					{title}
-				</Link>
-				<div className="flex justify-center mt-2">
-					<h4 className="text-xl font-bold text-greenBtn">£20.00</h4>
-					<h4 className="text-xl font-bold text-greenBtn line-through ml-3 opacity-60">
-						$25.00
-					</h4>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
--- src/redux/products/productSlice.js
+++ src/redux/products/productSlice.js
@@ -1,100 +1,26 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import apiAxios from '../../services/apiAxios';
+import axios from 'axios';
 
-export const getProducts = createAsyncThunk(
-	'productDisplay/getProducts',
-	async () => {
-		try {
-			const res = await apiAxios.get('/search?query=vegetables');
-			// const res = await apiAxios.get(`/search?query=${productName}`);
-			console.log(res);
-		} catch (err) {
-			console.log('Error getting products failed: ' + err.message);
-		}
-	}
-);
+const API_KEY = `${process.env.REACT_APP_API_KEY_2}`;
+const getProducts = (setProVeg, setProBread,setProFruits) => {
+	try {
+		let endpoints = [
+			`https://api.spoonacular.com/food/products/search?apiKey=${API_KEY}&query=vegetables`,
+			`https://api.spoonacular.com/food/products/search?apiKey=${API_KEY}&query=bread`,
+			`https://api.spoonacular.com/food/products/search?apiKey=${API_KEY}&query=fruits`,
+		];
 
-export const getProductDetail = createAsyncThunk(
-	'productDisplay/getProductDetail',
-	async (id) => {
-		try {
-			const res = await apiAxios.get(`/products/${id}`);
-			// console.log(res);
-			return res;
-		} catch (err) {
-			console.log('Error getting each product failed:', err.message);
-		}
-	}
-);
-
-const initialState = {
-	products: [],
-	isLoading: false,
-	filterProduct: [],
-	selectedProduct: {},
-};
-
-const productSlice = createSlice({
-	name: 'productDisplay',
-	initialState: initialState,
-	reducers: {
-		filterProductCategory: (state, action) => {
-			switch (action.payload) {
-				case 'FOOD & DRINKS':
-					state.filterProduct = state.products.filter(
-						(item) => item.category === 'Food_Drink'
-					);
-					break;
-				case 'VEGETABLES':
-					state.filterProduct = state.products.filter(
-						(item) => item.category === 'Vegetables'
-					);
-					break;
-				case 'DRIED FOODS':
-					state.filterProduct = state.products.filter(
-						(item) => item.category === 'DriedFood'
-					);
-					break;
-				case 'BREAD & CAKE':
-					state.filterProduct = state.products.filter(
-						(item) => item.category === 'Bread_Cake'
-					);
-					break;
-				case 'FISH & MEAT':
-					state.filterProduct = state.products.filter(
-						(item) => item.category === 'Fish_Meat'
-					);
-					break;
-				default:
-					return state;
+		Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+			(res) => {
+				console.log(res[0].data);
+				let proArray = []
+				const productList = proArray.push(res[0].data.products);
+				console.log("productLis",proArray)
+				setProVeg(productList);
+				setProBread()
 			}
-		},
-	},
-	extraReducers: (builder) => {
-		builder
-			.addCase(getProducts.pending, (state) => {
-				state.isLoading = true;
-			})
-			.addCase(getProducts.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.products = action.payload;
-				state.filterProduct = action.payload;
-			})
-			.addCase(getProducts.rejected, (state) => {
-				state.isLoading = false;
-			})
-			.addCase(getProductDetail.pending, (state) => {
-				state.isLoading = true;
-			})
-			.addCase(getProductDetail.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.selectedProduct = action.payload;
-			})
-			.addCase(getProductDetail.rejected, (state) => {
-				state.isLoading = false;
-			});
-	},
-});
-
-export const productActions = productSlice.actions;
-export default productSlice;
+		);
+	} catch (err) {
+		console.log('Error getting products failed: ' + err.message);
+	}
+};
+export default getProducts;
