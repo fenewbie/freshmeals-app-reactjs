20c34b4dab460c90b801dbfa89bc40059df60984
Pham Mi <huynhmi1209@gmail.com>
2022-11-12 22:25:15 +0700

    fix blog

--- src/components/UI/Blog/BlogCard.js
+++ src/components/UI/Blog/BlogCard.js
@@ -1,43 +1,46 @@
 import {FaTags, FaUserEdit} from 'react-icons/fa';
 import {BiCalendar} from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import Button from '../Button';
 
-const BlogCard = () => {
-    return (
-        <div className="shadow-xl rounded-lg overflow-hidden my-10">
-            <Link to='/' className='overflow-hidden inline-block'>
-                <img alt='Blog' src='https://tunatheme.com/tf/html/broccoli-preview/broccoli/img/blog/1.jpg' className='transition-all duration-500 scale-100 hover:scale-110'/>
+const BlogCard = ({title = 'Title', description = 'Write something here...', img, tags = []}) => (
+        <div className="shadow-xl rounded-lg overflow-hidden my-10 min-h-[650px]">
+            <Link className='pb-[70%] w-full overflow-hidden relative flex bg-slate-50'>
+                <div className='absolute top-0 left-0 h-full w-full'>
+                    <img alt='Blog' src={img} className='mx-auto w-auto h-full transition-all duration-500 scale-100 hover:scale-110'/>
+                </div>
             </Link>
             <div className="bg-white p-8 ">
                 <div className='flex mb-3'>
                     
                     <div className='flex items-center'>
                         <FaTags className='text-greenBtn'/>
-                        <Link to='/' className='text-sm font-medium py-1 px-5 rounded-full border border-greenBtn inline-block text-center bg-transparent ml-2 transition-all duration-300 hover:text-white hover:bg-greenBtn'>
-                            Service
-                        </Link>
+                        {
+                            tags.map((tag, index) => (
+                                <Link to='/' key={index} className='text-sm font-medium py-1 px-4 rounded-full border border-greenBtn inline-block text-center bg-transparent ml-2 transition-all duration-300 hover:text-white hover:bg-greenBtn shadow-md'>
+                                    {tag}
+                                </Link>
+                            ))
+                        }
                     </div>
                 </div>
-                <Link class="text-xl lg:text-2xl font-bold capitalize inline-block mb-2 hover:text-greenBtn transition-all duration-300">Common Engine Oil problems and solutions</Link>
-                <p className='text-justify'>
-                    Due to its central geographic location in Southern Europe, Italy has historically been home to myriad peoples and cultures. In addition to the various ancient peoples dispersed throughout what is now modern-day Italy
-                    <Link to='/' className='text-greenBtn font-medium underline underline-offset-4 ml-1'>Read more</Link>
+                <Link className="text-xl lg:text-2xl font-bold capitalize inline-block mb-2 hover:text-greenBtn transition-all duration-300">{title}</Link>
+                <p className='text-justify truncate'>
+                    {description}
                 </p>
+                    <Link to='/' className='text-greenBtn font-medium underline underline-offset-4'>Read more</Link>
                 
                 <div className='flex items-center justify-between mt-7 border-t pt-6'>
                     <Link to='/' className='flex items-center hover:text-greenBtn transition-all duration-300'>
                         <FaUserEdit className='text-greenBtn mr-2'/>
-                        by <span className='ml-1 font-medium'>Admin</span>
+                        by <span className='ml-1 font-medium capitalize'>admin</span>
                     </Link>
                     <span className='flex items-center'> 
                         <BiCalendar className='text-greenBtn mr-2'/>
-                        June 24, 2022
+                        Nov 12, 2022
                     </span>
                 </div>
             </div>
         </div>
     )
-}
 
 export default BlogCard;
--- src/components/UI/Blog/index.js
+++ src/components/UI/Blog/index.js
@@ -1,14 +1,30 @@
-import { SwiperSlide } from "swiper/react";
-import BlogCard from "./BlogCard";
-import BlogSwiper from "./BlogSwiper";
+import { useState, useEffect } from "react";
+import {db} from '../../../services/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
+import { Swiper ,SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+
+import BlogCard from "./BlogCard";
 import SliderButton from '../Slider/SliderButton';
-import { useState } from "react";
 
 const Blog = () => {
+    const [blogImgs, setBlogImgs] = useState([]);
     const [toggleBtn, setToggleBtn] = useState(false);
-    
-    
+
+    useEffect(() => {
+        const getImgs = async () => {
+        const docRef = collection(db, 'blog');
+        const resp = await getDocs(docRef);
+        setBlogImgs(resp.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+        };
+        getImgs();
+    }, []);
+
+    console.log(blogImgs);
     return (
         <div className="my-8"
             onMouseEnter={() => {
@@ -18,32 +34,61 @@ const Blog = () => {
                 setToggleBtn(false);
             }}
         >
-            <BlogSwiper>
-                <SliderButton
-						isNext={false}
-						iconSize={36}
-						iconColors={['white', '#80B500']}
-						className={`p-3 bg-transparent rounded border-gray-400 border-2 hover:border-white hover:bg-greenBtn ${
-							toggleBtn ? 'visible translate-x-[0%] opacity-100' : 'invisible -translate-x-[30%] opacity-0'
-						} transition-all ease-in-out duration-300 lg:block hidden`}
-						iconClassName={`transition-all ease-in-out duration-300`}
-				/>
-                <SwiperSlide><BlogCard/></SwiperSlide>
-                <SwiperSlide><BlogCard/></SwiperSlide>
-                <SwiperSlide><BlogCard/></SwiperSlide>
-                <SwiperSlide><BlogCard/></SwiperSlide>
-                <SwiperSlide><BlogCard/></SwiperSlide>
-                <SwiperSlide><BlogCard/></SwiperSlide>
-                <SliderButton
-						isNext={true}
-						iconSize={36}
-						iconColors={['white', '#80B500']}
-						className={`p-3 bg-transparent rounded border-gray-400 border-2 hover:border-white hover:bg-greenBtn ${
-							toggleBtn ? 'visible translate-x-[0%] opacity-100' : 'invisible -translate-x-[30%] opacity-0'
-						} transition-all ease-in-out duration-300 lg:block hidden`}
-						iconClassName={`transition-all ease-in-out duration-300`}
-				/>
-            </BlogSwiper>
+
+            {
+                blogImgs.length > 0 && 
+                <Swiper 
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    loop={true}
+                    pagination={{
+                    clickable: true,
+                    }}
+                    modules={[Pagination,Navigation]}
+                
+                    className="mySwiper"
+                >
+
+                    <SliderButton
+                        isNext={false}
+                        iconSize={30}
+                        iconColors={['white', '#80B500']}
+                        className={`p-2 bg-white rounded-full border-gray-200 border-2 hover:bg-greenBtn ${
+                            toggleBtn ? 
+                                'visible translate-x-[0%] opacity-100' : 
+                                'invisible -translate-x-[30%] opacity-0' 
+                        } shadow-2xl transition-all ease-in-out duration-300 lg:block hidden`}
+                        iconClassName={`transition-all ease-in-out duration-300`}
+                    /> 
+                    <SliderButton
+                        isNext={true}
+                        iconSize={30}
+                        iconColors={['white', '#80B500']}
+                        className={`p-2 bg-white rounded-full border-gray-200 border-2 hover:bg-greenBtn ${
+                            toggleBtn ? 
+                                'visible translate-x-[0%] opacity-100' : 
+                                'invisible translate-x-[30%] opacity-0'
+                        } shadow-2xl transition-all ease-in-out duration-300 lg:block hidden`}
+                        iconClassName={`transition-all ease-in-out duration-300`}
+                    /> 
+
+                    {
+                        blogImgs.map(blogItem => (
+                            <SwiperSlide key={blogItem.id}>
+                                <BlogCard 
+                                    title={blogItem.title} 
+                                    description={blogItem.description} 
+                                    tags={blogItem.tags} 
+                                    img={blogItem.image}
+                                />
+                            </SwiperSlide>
+                        ))
+                    }
+
+                    
+                </Swiper>
+            }
+
         </div>
     )
 }
