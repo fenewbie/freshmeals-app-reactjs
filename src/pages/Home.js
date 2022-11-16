4e1c1f0a1e6ce015f74a08c2c3c956ea1c77d983
Pham Mi <huynhmi1209@gmail.com>
2022-11-16 15:34:06 +0700

    add ProductDetail from Blog without merging

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,13 @@
 import MainLayout from '../components/Layout/MainLayout';
 import Promotion from '../components/UI/Promotion';
 import CountDown from '../components/UI/CountDown'
-import Blog from '../components/UI/Blog';
-import Test from '../components/UI/Tabs/TestTabs';
 import ProductDetail from '../components/UI/ProductDetails';
 
 const Home = () => {
   return (
     <MainLayout>
       <Promotion/>
-      <div className='my-20'>
-        <Test/>
-      </div>
       <CountDown/>
-      <Blog/>
       <ProductDetail/>
     </MainLayout>
   );
