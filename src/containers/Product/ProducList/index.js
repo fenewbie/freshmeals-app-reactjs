15c89c55775c24e096ea20a37f0913fccc92025d
gonin <goninha@outlook.fr>
2022-11-28 15:09:42 +0000

    fix tabs menu

--- src/containers/Product/ProducList/index.js
+++ src/containers/Product/ProducList/index.js
@@ -1,17 +1,15 @@
+import { useSelector } from 'react-redux';
 import { SwiperSlide } from 'swiper/react';
-import { useState } from 'react';
 import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 
 import useFirestore from '../../../hooks/useFirestore';
-import { useSelector } from 'react-redux';
 
 import ProductItem from '../ProductItem';
-// import Tabs from '../../../components/UI/Tabs/Tabs';
 import Slider from '../../../components/UI/Slider';
 import QuickViewProductModal from '../QuickViewProductModal';
 import SuccessModal from '../SuccessModal';
 import Title from '../../../components/Title';
-import Swiper from 'swiper';
+import { labelProduct as label } from '../../../constants/Constant';
 
 const ProductList = () => {
 	const { docs } = useFirestore('products');
@@ -28,28 +26,19 @@ const ProductList = () => {
 			<Title title="Our Products" />
 			<div className="mb-[15px]">
 				<Tabs>
-					<TabList className="flex justify-center flex-wrap pb-6">
-						<Tab
-							className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 border-b-2 text-greenBtn border-greenBtn
-									 hover:text-greenBtn"
-						>
-							Food & Drinks
-						</Tab>
-						<Tab className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 border-b-2 text-greenBtn border-transparent hover:text-greenBtn">
-							Vegetables
-						</Tab>
-						<Tab className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 border-b-2 text-greenBtn border-transparent hover:text-greenBtn">
-							Bread & Cake
-						</Tab>
-						<Tab className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 border-b-2 text-greenBtn border-transparent hover:text-greenBtn">
-							Dried Food
-						</Tab>
-						<Tab className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 border-b-2 text-greenBtn border-transparent hover:text-greenBtn">
-							Fish & Meat
-						</Tab>
-						<Tab className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 border-b-2 text-greenBtn border-transparent hover:text-greenBtn">
-							Fruits
-						</Tab>
+					<TabList className="flex justify-center flex-wrap pb-6 relative">
+						{label.map((label) => (
+							<Tab
+								className="text-sm md:text-lg font-bold uppercase px-6 py-4 my-1 mx-2 
+									 hover:text-greenBtn cursor-pointer 
+									 before:absolute
+									 before:content-['|'] before:h-6 before:w-1 
+									 before:bg-[#8cb2b2]"
+								selectedClassName="border-greenBtn border-b-2 text-greenBtn outline-0 hover:cursor-pointer"
+							>
+								{label.label}
+							</Tab>
+						))}
 					</TabList>
 
 					<TabPanel>
