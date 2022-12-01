27c400b6640e398fb83802ce54d47e8f02dd71b1
Pham Mi <huynhmi1209@gmail.com>
2022-12-01 17:09:40 +0700

    delete TopRatedProductCard and edit ProductItem

--- src/containers/Product/ProductItem/Rating.js
+++ src/containers/Product/ProductItem/Rating.js
@@ -2,9 +2,9 @@ import { RiStarFill } from 'react-icons/ri';
 import { RiStarHalfFill } from 'react-icons/ri';
 import { RiStarLine } from 'react-icons/ri';
 
-const Rating = ({ value, text, color = '#FFB800', size = 18 }) => {
+const Rating = ({ value, text, color = '#FFB800', size = 18, center = false }) => {
 	return (
-		<div className='flex items-center'
+		<div className={`flex items-center ${center && 'justify-center'}`}
 		>
 			<span>
 				{value >= 1 ? (
