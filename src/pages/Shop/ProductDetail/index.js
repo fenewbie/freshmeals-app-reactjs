f767720b05e1eb2de703e03cc19fdc014fd9e06c
Pham Mi <huynhmi1209@gmail.com>
2022-12-01 19:43:02 +0700

    add PromotionCard and add Promotion in product-detail page

--- src/pages/Shop/ProductDetail/index.js
+++ src/pages/Shop/ProductDetail/index.js
@@ -4,16 +4,27 @@ import * as cs from '../../../constants/Constant';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useFetchDocument from '../../../hooks/useFetchDocument';
+import useFirestore from '../../../hooks/useFirestore';
+
 import Loading from '../../../components/UI/Loading';
 import ProductArea from './ProductArea';
 import ProductRelated from '../../../containers/Product/ProductRelated';
-
+import PromotionCard from '../../../containers/Promotion/PromotionCard';
 import ProductTopRated from '../../../containers/Product/ProductTopRated';
+
 function ProductDetail() {
 	const [product, setProduct] = useState(null);
 	let { productId } = useParams();
 	const { document } = useFetchDocument('products', productId);
 
+	const { docs } = useFirestore('promotion');
+	const [promotion, setPromotion] = useState();
+	
+	useEffect(() => {
+		const random = Math.floor(Math.random() * docs.length);
+		setPromotion(docs[random]);
+	}, [docs]);
+	
 	useEffect(() => {
 		setProduct(document);
 	}, [document]);
@@ -30,7 +41,16 @@ function ProductDetail() {
 						</div>
 						<div className="lg:col-span-4 ">
 							<ProductTopRated />
-							<div></div>
+							<div className="mt-10">
+								{promotion && (
+									<PromotionCard
+										type={promotion.type}
+										title={promotion.title}
+										subtitle={promotion.subtitle}
+										image={promotion.image}
+									/>
+								)}
+							</div>
 						</div>
 					</div>
 
