5a28037930845dcf8737469376ea2c20af8912da
Pham Mi <huynhmi1209@gmail.com>
2022-12-18 14:12:13 +0700

    fix css pagiantion

--- src/components/UI/Pagination/index.js
+++ src/components/UI/Pagination/index.js
@@ -5,19 +5,21 @@ function Pagination({ totalPages, currentPage, handlePageChange }) {
 	}
 
 	return (
-		<div className="flex justify-center items-center">
+		<div className="flex justify-center items-center flex-wrap">
 			<button
 				disabled={currentPage === 1}
-				className={`py-2 px-4 mx-5 border rounded  ${
-					currentPage !== 1 ? 'cursor-pointer' : 'opacity-60'
-				}  ${totalPages === 1 ? 'invisible' : 'visible'}`}
+				className={`py-2 px-4 mx-5 border rounded mb-2  ${
+					currentPage !== 1
+						? 'cursor-pointer hover:border-greenBtn'
+						: 'opacity-60'
+				}  ${totalPages < 2 ? 'invisible' : 'visible'}`}
 				onClick={() => handlePageChange(currentPage - 1)}
 			>
 				Previous
 			</button>
 			{pageNumberBtns.map((numberBtn, index) => (
 				<button
-					className={`h-10 w-10 rounded border mx-2 ${
+					className={`h-10 w-10 rounded border mx-2 mb-2 hover:border-greenBtn ${
 						currentPage === numberBtn
 							? 'border-greenBtn bg-greenBtn text-white'
 							: ''
@@ -31,9 +33,11 @@ function Pagination({ totalPages, currentPage, handlePageChange }) {
 			<button
 				disabled={currentPage === totalPages}
 				onClick={() => handlePageChange(currentPage + 1)}
-				className={`py-2 px-4 mx-5 border rounded  ${
-					currentPage < totalPages ? 'cursor-pointer' : 'opacity-60'
-				} ${totalPages === 1 ? 'invisible' : 'visible'}`}
+				className={`py-2 px-4 mx-5 border rounded mb-2  ${
+					currentPage < totalPages
+						? 'cursor-pointer hover:border-greenBtn'
+						: 'opacity-60'
+				} ${totalPages < 2 ? 'invisible' : 'visible'}`}
 			>
 				Next
 			</button>
