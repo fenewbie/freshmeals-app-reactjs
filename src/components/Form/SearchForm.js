6ab34f5e107a9423b105986a5aa8d09830b32507
Pham Mi <huynhmi1209@gmail.com>
2022-12-26 22:49:42 +0700

    replaced search form with formik

--- src/components/Form/SearchForm.js
+++ src/components/Form/SearchForm.js
@@ -1,51 +1,50 @@
 import { BsSearch } from 'react-icons/bs';
 
 import { useRef } from 'react';
+import { Form, Formik } from 'formik';
+import FormikControl from './FormikControl';
 
 function SearchForm({ setSearchKey }) {
-	const inputRef = useRef();
-
 	const timer = useRef();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const value =
-			inputRef.current.value.trim().length > 0
-				? inputRef.current.value
-				: null;
+	const handleSubmit = (values) => {
+		let value = values.search.trim();
+		value = value.length > 0 ? value : null;
 		setSearchKey({ value: value });
 	};
 
-	const handleChange = (e) => {
+	const handleChangeCustom = (e) => {
 		if (timer.current) {
 			clearTimeout(timer.current);
 		}
 
 		timer.current = setTimeout(() => {
-			const value =
-				e.target.value.trim().length > 0 ? e.target.value : null;
+			let value = e.target.value.trim();
+			value = value.length > 0 ? value : null;
 			setSearchKey({ value: value });
 		}, 200);
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className="flex items-center border-2 rounded-md py-4 px-4 focus-within:border-greenBtn">
-				<input
-					id="search"
-					name="search"
-					type="text"
-					placeholder="Looking for some things..."
-					className="border-none outline-none w-full flex-1"
-					ref={inputRef}
-					onChange={handleChange}
-				/>
-
-				<span className="ml-4">
-					<BsSearch className="mx-auto" />
-				</span>
-			</div>
-		</form>
+		<Formik
+			initialValues={{ search: '' }}
+			onSubmit={handleSubmit}
+		>
+			{({ handleChange }) => (
+				<Form>
+					<FormikControl
+						control="input"
+						name="search"
+						placeholder="Enter your search key..."
+						onChange={(e) => {
+							handleChangeCustom(e);
+							handleChange(e);
+						}}
+						icon={<BsSearch />}
+					/>
+				</Form>
+			)}
+		</Formik>
 	);
 }
 
