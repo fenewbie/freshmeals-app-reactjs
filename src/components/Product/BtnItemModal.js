2871e024baf8979cf8d55b797ee6f417af7933de
Pham Mi <huynhmi1209@gmail.com>
2023-01-06 09:49:02 +0700

    fix BtnIconItem of ProductItem

--- src/components/Product/BtnItemModal.js
+++ src/components/Product/BtnItemModal.js
@@ -7,7 +7,7 @@ export default function BtnIconItem({
 	handleOpenWishListModal,
 }) {
 	return (
-		<div className="absolute left-0 top-1/2 w-full text-center opacity-0 transition-all duration-300 translate-y-1/2 group-hover:opacity-100 group-hover:-translate-y-1/2">
+		<div className="absolute left-0 top-1/2 w-full text-center opacity-0 invisible transition-all duration-300 translate-y-1/2 group-hover:opacity-100 group-hover:-translate-y-1/2 group-hover:visible">
 			<button
 				className="md:h-[50px] md:w-[50px] w-10 h-10 border max-md:text-sm font-medium text-gray-900 bg-white rounded-full focus:outline-none hover:bg-greenBtn hover:text-white transition-all duration-300 
 				mx-2"
