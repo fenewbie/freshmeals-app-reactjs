6550b523d3d8c9ec89911446f13276a1788478d8
Pham Mi <huynhmi1209@gmail.com>
2023-01-12 08:38:48 +0700

    add load while searching

--- src/components/Form/SearchForm.js
+++ src/components/Form/SearchForm.js
@@ -1,11 +1,14 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Form, Formik } from 'formik';
-import { BsSearch } from 'react-icons/bs';
 
 import FormikControl from './FormikControl';
 
+import { BsSearch } from 'react-icons/bs';
+import { BiLoader } from 'react-icons/bi';
+
 function SearchForm({ setSearchKey }) {
 	const timer = useRef();
+	const [isTyping, setIsTyping] = useState(false);
 
 	const handleSubmit = (values) => {
 		let value = values.search.trim();
@@ -14,6 +17,7 @@ function SearchForm({ setSearchKey }) {
 	};
 
 	const handleChangeCustom = (e) => {
+		setIsTyping(true);
 		if (timer.current) {
 			clearTimeout(timer.current);
 		}
@@ -22,29 +26,32 @@ function SearchForm({ setSearchKey }) {
 			let value = e.target.value.trim();
 			value = value.length > 0 ? value : null;
 			setSearchKey({ value: value });
-		}, 100);
+			setIsTyping(false);
+		}, 800);
 	};
 
 	return (
-		<Formik
-			initialValues={{ search: '' }}
-			onSubmit={handleSubmit}
-		>
-			{({ handleChange }) => (
-				<Form>
-					<FormikControl
-						control="input"
-						name="search"
-						placeholder="Enter your search key..."
-						onChange={(e) => {
-							handleChangeCustom(e);
-							handleChange(e);
-						}}
-						icon={<BsSearch />}
-					/>
-				</Form>
-			)}
-		</Formik>
+		<>
+			<Formik
+				initialValues={{ search: '' }}
+				onSubmit={handleSubmit}
+			>
+				{({ handleChange }) => (
+					<Form>
+						<FormikControl
+							control="input"
+							name="search"
+							placeholder="Enter your search key..."
+							onChange={(e) => {
+								handleChangeCustom(e);
+								handleChange(e);
+							}}
+							icon={isTyping ? <BiLoader /> : <BsSearch />}
+						/>
+					</Form>
+				)}
+			</Formik>
+		</>
 	);
 }
 
